fe20161c662fd196c7fe3a295f30679dd8b6675f
Suryavamsi Tenneti <suryavamsi8@gmail.com>
2020-05-06 17:57:22 -0700

    Modifications to the models

--- mysite/hospital_management/admin.py
+++ mysite/hospital_management/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import User, Doctor, Patient
 
 # Register your models here.
+
+
+class UserAdminView(admin.ModelAdmin):
+    list_display = ('name', 'user_id')
+
+
+admin.site.register(User, UserAdminView)
+admin.site.register(Doctor)
+admin.site.register(Patient)
--- mysite/hospital_management/models.py
+++ mysite/hospital_management/models.py
@@ -6,40 +6,34 @@ import datetime
 # Create your models here.
 
 
-class Users(models.Model):
+class User(models.Model):
     user_id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=50)
-    date_of_joining = models.DateTimeField('Joining date')
+    date_of_joining = models.DateField('Joining date')
     phone_regex = RegexValidator(regex=r'^\+?1?\d{9,15}$',
                                  message="Phone number must be entered in the format: '+999999999'."
                                          " Up to 15 digits allowed.")
-    phone = models.CharField(validators=[phone_regex], max_length=17, blank=True)
+    phone = models.CharField(validators=[phone_regex], max_length=17)
     address = models.CharField(max_length=100)
+    username = models.CharField(max_length=15)
+    password = models.CharField(max_length=15)
 
     def __str__(self):
         return self.name
 
 
-class Accounts(models.Model):
-    user_id = models.ForeignKey(Users, on_delete=models.CASCADE)
-    username = models.CharField(max_length=15)
-    password = models.CharField(max_length=15)
-
-    def __str__(self):
-        return self.user_id + " : " + self.username
-
-
-class Doctors(models.Model):
-    user_id = models.ForeignKey(Users, on_delete=models.CASCADE)
+class Doctor(models.Model):
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
     department = models.CharField(max_length=50)
 
     def __str__(self):
-        return self.name + " : " + self.department
+        name = User.objects.get(pk=self.user_id)
+        return name + " : " + self.department
 
 
 class Patient(models.Model):
     patient_id = models.AutoField(primary_key=True)
-    assigned_doctor = models.ForeignKey(Doctors, on_delete=models.CASCADE)
+    assigned_doctor = models.ForeignKey(Doctor, on_delete=models.CASCADE)
     name = models.CharField(max_length=50)
     age = models.IntegerField()
     weight = models.IntegerField()
